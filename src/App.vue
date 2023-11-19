1631a9ae6808b583fa8a27060fdd6ebf146b92f8
jorge-tarin <jorge.tariin@gmail.com>
2023-11-19 17:27:57 -0300

    implementation of the header and lanes

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,40 @@
 <script setup>
+  import Header from "./components/Header.vue"
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
+  <Header />
+  <div class="board">
+    <div class="lane">
+      <h2 class="lane-title">Backlog</h2>
     </div>
-  </header>
-
-  <main>
-  </main>
+    <div class="lane">
+      <h2 class="lane-title">Desenvolvimento</h2>
+    </div>
+    <div class="lane">
+      <h2 class="lane-title">Testes</h2>
+    </div>
+    <div class="lane">
+      <h2 class="lane-title">Fechados</h2>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.board{
+  display: flex;
+  justify-content: flex-start;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.lane{
+  background: var(--color-gray);
+  width: 23rem;
+  height: 30rem;
+  border-radius: 0.8rem;
+  box-shadow: 0 0.1rem 0.2rem 0 rgba(33, 33, 33, 0.1);
+  margin: 0.8rem;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.lane-title{
+  margin-bottom: 0.6rem;
+  padding: 0.8rem 0.5rem;
 }
 </style>
